b1b9c8ef74c5e24bfa5a6eb0364df8164cde023b
talitast <talitatravassos.tt@gmail.com>
2022-03-02 11:56:40 -0300

    fix some things

--- src/components/HeroCard/index.tsx
+++ src/components/HeroCard/index.tsx
@@ -1,20 +1,26 @@
 import React from "react";
-import styles from "./styles.module.scss";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { getHero } from "../../requests";
 import { Hero } from "../../typings/hero.types";
+import styles from "./styles.module.scss";
 
 interface Props {
   hero: Hero;
 }
 
 export default function HeroCard({ hero }: Props) {
+  const dispatch = useDispatch();
+
   return (
     <Link
-      to={{ pathname: `/details/${hero.name.replace(/\s/g, '').toLowerCase()}`, state: { _id: `${hero.id}` } }}
+      to={{
+        pathname: `/details/${hero.name.replace(/\s/g, "").toLowerCase()}`,
+      }}
       style={{ textDecoration: "none" }}
     >
-      <div className={styles.card}>
-        <img src={hero.image.url} alt={hero.name} height="350" />
+      <div className={styles.card} onClick={() => dispatch(getHero(hero.id))}>
+        <img src={hero.image.url} alt={hero.name} height='350' />
         <div className={styles.info_container}>
           <h3>{hero.name}</h3>
         </div>
--- src/views/Details/index.tsx
+++ src/views/Details/index.tsx
@@ -1,22 +1,12 @@
 import React from "react";
-import { RouteComponentProps } from "react-router";
-import { useSelector, useDispatch } from "react-redux";
-import { getHero } from "../../requests";
-import styles from "./styles.module.scss";
-import { Hero } from "../../typings/hero.types";
+import { useSelector } from "react-redux";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
+import { Hero } from "../../typings/hero.types";
+import styles from "./styles.module.scss";
 
-export default function Details(props: RouteComponentProps) {
-  const dispatch = useDispatch();
+export default function Details() {
   const hero: Hero = useSelector((state: any) => state.heroes.selectHero);
-
-  React.useEffect(() => {
-    const { _id } = props.location.state;
-
-    dispatch(getHero(_id));
-    // eslint-disable-next-line
-  }, []);
 
   React.useEffect(() => {
     // eslint-disable-next-line
@@ -26,63 +16,57 @@ export default function Details(props: RouteComponentProps) {
   return (
     <div className={styles.container}>
       <Navbar />
-      
-        {hero.image ? (
-          <div className={styles.container_details}>
-            <img
-              src={hero.image.url}
-              alt={hero.name}
-              className={styles.image}
-            />
-            <div className={styles.container_info}>
-              <h1>{hero.name}</h1>
-              <div>
-                <h3>Appearance</h3>
-                <p>
-                  Eye color: {hero.appearance["eye-color"]} | Gender:{" "}
-                  {hero.appearance["gender"]} | Hair color:{" "}
-                  {hero.appearance["hair-color"]} | Height:{" "}
-                  {hero.appearance["height"][0]} | Weight:{" "}
-                  {hero.appearance["weight"][0]}
-                </p>
-              </div>
-              <div className={styles.biography}>
-                <h3>Biography</h3>
-                <p>Full Name: {hero.biography["full-name"]}</p>
-                <p>Publisher: {hero.biography["publisher"]}</p>
-                <p>Alignment: {hero.biography["alignment"]}</p>
-                <p>Alter egos: {hero.biography["alter-egos"]}</p>
-                <p>First appearance: {hero.biography["first-appearance"]}</p>
-                <p>Place of birth: {hero.biography["place-of-birth"]}</p>
-              </div>
-              <div className={styles.biography}>
-                <h3>Powerstats</h3>
-                <p>
-                  Combat: {hero.powerstats["combat"]} | Durability:{" "}
-                  {hero.powerstats["durability"]} | Intelligence:{" "}
-                  {hero.powerstats["intelligence"]} | Power:{" "}
-                  {hero.powerstats["power"]} | Speed: {hero.powerstats["speed"]}
-                  | Strength: {hero.powerstats["strength"]}
-                </p>
-              </div>
-              <div className={styles.biography}>
-                <h3>Connections</h3>
-                <p>
-                  Group affiliation: {hero.connections["group-affiliation"]}
-                </p>
-                <p>Relatives: {hero.connections["relatives"]}</p>
-              </div>
-              <div className={styles.biography}>
-                <h3>Work</h3>
-                <p>Base: {hero.work["base"]}</p>
-                <p>Occupation: {hero.work["occupation"]}</p>
-              </div>
+
+      {hero.image ? (
+        <div className={styles.container_details}>
+          <img src={hero.image.url} alt={hero.name} className={styles.image} />
+          <div className={styles.container_info}>
+            <h1>{hero.name}</h1>
+            <div>
+              <h3>Appearance</h3>
+              <p>
+                Eye color: {hero.appearance["eye-color"]} | Gender:{" "}
+                {hero.appearance["gender"]} | Hair color:{" "}
+                {hero.appearance["hair-color"]} | Height:{" "}
+                {hero.appearance["height"][0]} | Weight:{" "}
+                {hero.appearance["weight"][0]}
+              </p>
             </div>
+            <div className={styles.biography}>
+              <h3>Biography</h3>
+              <p>Full Name: {hero.biography["full-name"]}</p>
+              <p>Publisher: {hero.biography["publisher"]}</p>
+              <p>Alignment: {hero.biography["alignment"]}</p>
+              <p>Alter egos: {hero.biography["alter-egos"]}</p>
+              <p>First appearance: {hero.biography["first-appearance"]}</p>
+              <p>Place of birth: {hero.biography["place-of-birth"]}</p>
             </div>
-        ) : (
-          ""
-        )}
-      
+            <div className={styles.biography}>
+              <h3>Powerstats</h3>
+              <p>
+                Combat: {hero.powerstats["combat"]} | Durability:{" "}
+                {hero.powerstats["durability"]} | Intelligence:{" "}
+                {hero.powerstats["intelligence"]} | Power:{" "}
+                {hero.powerstats["power"]} | Speed: {hero.powerstats["speed"]}|
+                Strength: {hero.powerstats["strength"]}
+              </p>
+            </div>
+            <div className={styles.biography}>
+              <h3>Connections</h3>
+              <p>Group affiliation: {hero.connections["group-affiliation"]}</p>
+              <p>Relatives: {hero.connections["relatives"]}</p>
+            </div>
+            <div className={styles.biography}>
+              <h3>Work</h3>
+              <p>Base: {hero.work["base"]}</p>
+              <p>Occupation: {hero.work["occupation"]}</p>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
       <Footer />
     </div>
   );
